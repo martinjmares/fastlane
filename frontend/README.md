98cba5fb5d3618492f67e451b9d9cf61e2724684
Martin Mares <martin.mares@oracle.com>
2016-12-14 14:36:28 +0100

    Add Bower Start UI

--- frontend/README.md
+++ frontend/README.md
@@ -5,6 +5,10 @@
 ### Login
 
 ```
-curl -H "Content-Type: application/json" -X POST -d '{"username":"bart","password":"simpson"}' http://localhost:8080/login
-curl -H "Content-Type: application/json" -H "fastlane-auth: 15392c38-7c44-4be3-b2e9-99aafcbdae51" -X GET http://localhost:8080/login
+curl -H "Content-Type: application/json" -X POST -d '{"username":"bart","password":"simpson"}' http://localhost:8080/api/login
+curl -H "Content-Type: application/json" -H "fastlane-auth: 15392c38-7c44-4be3-b2e9-99aafcbdae51" -X GET http://localhost:8080/api/login
 ```
+
+## Notes
+
+- Don't forgot to set NODE_ENV="production" in production
--- frontend/app.js
+++ frontend/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
 
@@ -15,18 +14,17 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public', 'index.html')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/login', login);
+app.use('/api/users', users);
+app.use('/api/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
